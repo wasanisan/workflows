e43b5915a2f262882d6a04fb0bd8f8b14702fc2e
Jamie Keomanivong <jamiek.design@gmail.com>
2016-03-06 12:16:39 -0600

    Fixed broken JSON data

--- components/scripts/template.js
+++ components/scripts/template.js
@@ -3,8 +3,15 @@ $(function() {
 
   $.getJSON('js/data.json', function(data) {
     var template = $('#speakerstpl').html();
-    var html = Mustache.to_html(template, data);
+    var html = Mustache.render(template, data);
     $('#speakers').html(html);    
+  }).done(function() {
+  	console.log('success!');
+  }).fail(function(jqxhr, textStatus, error) {
+  	var err = textStatus + ", " + error;
+    console.log( "Request Failed: " + err );
+  }).always(function() {
+  	console.log('complete!');
   }); //getJSON
   
 }); //function
